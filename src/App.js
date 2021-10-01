35c0fccb2835068a0a68e695c13575d9c05c447d
tpalee <thieulee@hotmail.com>
2021-10-01 10:19:06 +0200

    singlepokemon fetch app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const[pokeData,setPokeData]=useState(null);
+
+
+    useEffect(() => {
+        async function fetchData() {
+
+            try {
+                const result = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff');
+                setPokeData(result.data);
+                console.log(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+
+            fetchData();
+
+
+    }, []);
+
+
+
+
+    return (
+                    <div>
+                        <p>hoi</p>
+                        {pokeData &&
+                        <>
+                            <h3>{pokeData.name}</h3>
+                            <img src={pokeData.sprites.back_default} alt="pokeimage"/>
+                            <p>Moves: {pokeData.moves.length}</p>
+                            <p>weight: {pokeData.weight}</p>
+                            <p>abilities</p>
+                            {pokeData.abilities.map((ability)=>{
+                                return  <p key={ability.ability.name}>{ability.ability.name}</p>
+                            })}
+                        </>}
+
+                    </div>
+
+  )
 }
 
 export default App;
