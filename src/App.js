76ae74f87b5102efb354d3fdc4de4c734de69a47
tpalee <thieulee@hotmail.com>
2021-10-01 17:11:02 +0200

    everything works!

--- src/App.js
+++ src/App.js
@@ -1,50 +1,89 @@
-import React,{useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
+import PokemonInfo from "./components/PokemonInfo";
+import Button from "./components/Button";
+import pokemonlogo from "./assets/pokemonlogo.svg"
 
 function App() {
-    const[pokeData,setPokeData]=useState(null);
+    //Store the data in the state
+    const [allPokeData, setAllPokeData] = useState();
+    //Store the url in the state
+    const [pokeUrl, setPokeUrl] = useState(`https://pokeapi.co/api/v2/pokemon`);
+    //Store the disabled-state of the next-button
+    const [nextDisabled, setNextDisabled] = useState(false);
+    //Store the disabled-state of the previous-button
+    const [prevDisabled, setPrevDisabled] = useState(false);
+    //store the error-state
+    const [error, setError] = useState();
+
+    //nextbutton triggers nextclick function, previousbutton set to disabled
+    function nextClick() {
+        setPokeUrl(allPokeData.next);
+        setPrevDisabled(false);
+    }
+
+//prevbutton triggers prevClick Button, nextbutton set to disabled
+    function prevClick() {
+        setPokeUrl(allPokeData.previous);
+        setNextDisabled(false);
+    }
 
 
+//fetch data when pokeUrl changes
     useEffect(() => {
         async function fetchData() {
-
             try {
-                const result = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff');
-                setPokeData(result.data);
-                console.log(result.data);
+                const result = await axios.get(pokeUrl);
+                console.log("data is fetched");
+                setAllPokeData(result.data);
+
             } catch (e) {
                 console.error(e);
+                setError(true);
             }
         }
 
-
-            fetchData();
-
-
-    }, []);
-
-
+        fetchData();
+    }, [pokeUrl]);
 
 
     return (
-                    <div>
-                        <p>hoi</p>
-                        {pokeData &&
-                        <>
-                            <h3>{pokeData.name}</h3>
-                            <img src={pokeData.sprites.back_default} alt="pokeimage"/>
-                            <p>Moves: {pokeData.moves.length}</p>
-                            <p>weight: {pokeData.weight}</p>
-                            <p>abilities</p>
-                            {pokeData.abilities.map((ability)=>{
-                                return  <p key={ability.ability.name}>{ability.ability.name}</p>
-                            })}
-                        </>}
+        <>
+            <div className="wrappingcontainer">
+                {allPokeData && (
+                    <header>
+                        <img src={pokemonlogo} alt="pokemonlogo"/>
+                        <nav>
+                            <Button
+                                name="previous"
+                                disabled={!allPokeData.previous}
+                                onClick={prevClick}
+                            />
+                            <Button
+                                name="next"
+                                disabled={!allPokeData.next}
+                                onClick={nextClick}
+                            />
+                        </nav>
+                    </header>
+                )}
+                <main>
+                    {/*map the results and create for each result an PokemonInfo component, add it to the DOM*/}
+                    {allPokeData && allPokeData.results.map((result) => {
+                        return <PokemonInfo key={result.name} pokemon={result.name}/>
+                    })}
+                </main>
+                {/* log error in the dom*/}
+                {error && (
+                    <span className="error">
+                  Oh dear, data can't be fetched, please try again later!
+                </span>
 
-                    </div>
-
-  )
+                )}
+            </div>
+        </>
+    )
 }
 
 export default App;
